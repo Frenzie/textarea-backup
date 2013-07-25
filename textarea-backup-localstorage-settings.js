a8ad441f0c418dd491416e13fca433cc94165d6d
Frans de Jonge <fransdejonge@gmail.com>
2013-07-25 12:06:36 +0200

    Added sample settings file.

---
+++ textarea-backup-localstorage-settings.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name		Textarea Backup Localstorage Settings
+// @author		Frenzie
+// @include		*
+// ==/UserScript==
+
+window.opera.UJSTextareaBackupSettings = {
+	keep_after_submission : false,
+	backup_DOMNodeInserted : true,
+};
